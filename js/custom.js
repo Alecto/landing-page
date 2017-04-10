161df8f7fc5ac8410102ee7f5cc62018a1466aa9
Alecto <fomenkoandrey@inbox.ru>
2017-04-10 21:30:28 +0300

    about, features, gallery

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+(function ($) {  
+
+	/* bootstrap menu hover */
+	$(".dropdown").hover(            
+		function() {
+			$('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
+			$(this).toggleClass('open');
+			$('b', this).toggleClass("caret caret-up");                
+		},
+		function() {
+			$('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
+			$(this).toggleClass('open');
+			$('b', this).toggleClass("caret caret-up");                
+		});
+
+
+		$(window).scroll(function(){
+	    var t_scroll = $(window).scrollTop();
+	    var t_sticky = $('#navbar');
+
+			if (t_scroll >= 100) t_sticky.addClass('navbar-transparent');
+	  		else t_sticky.removeClass('navbar-transparent');
+		});
+    
+})(jQuery);                                                                 // Avoid conflicts with other libraries
