acdcefa835b5d3104549f5c7cfc2b433e3d4c80e
Andrey Fomenko <fomenkoandrey1978@gmail.com>
2019-04-22 20:13:55 +0300

    Конечный вариант шапки: слайдер + меню.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,12 @@
-(function ($) {  
+(function ($) {
 
+  $(window).scroll(() => {
+    let $navbar = $('#header-navbar');
+    const currentScrollPos = $(window).scrollTop();
+    const headerClassOnScroll = 'header-navbar--scrolled';
+    const scrollPos = 100;
+
+    currentScrollPos > scrollPos ? $navbar.addClass(headerClassOnScroll) : $navbar.removeClass(headerClassOnScroll);
+  });
 
 })(jQuery);
