2f71ca20194a4b6d44f844e1be52313ae50a5795
Andrey Fomenko <fomenkoandrey1978@gmail.com>
2019-04-23 20:45:16 +0300

    Настроена внутристраничная навигация.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,21 @@
 (function ($) {
+  let $navbar = $('#header-navbar');
 
   $(window).scroll(() => {
-    let $navbar = $('#header-navbar');
+
+
     const currentScrollPos = $(window).scrollTop();
     const headerClassOnScroll = 'header-navbar--scrolled';
     const scrollPos = 100;
 
     currentScrollPos > scrollPos ? $navbar.addClass(headerClassOnScroll) : $navbar.removeClass(headerClassOnScroll);
+  });
+
+  $navbar.onePageNav({
+    currentClass: 'active',
+    scrollSpeed: 750,
+    easing: 'swing',
+    filter: ':not(.navbar-brand)'
   });
 
 })(jQuery);
