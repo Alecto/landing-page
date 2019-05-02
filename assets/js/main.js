302bbb63c48a290a16f438c98a573755a858f241
Andrey Fomenko <fomenkoandrey1978@gmail.com>
2019-05-02 16:36:28 +0300

    Настройка анимации секции Blog.

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,8 +38,9 @@
 
   /* подключение и настрока плагина анимации */
   AOS.init({
-    duration: 500,
-    easing: 'ease-in-sine',
+    disable: 'mobile',
+    duration: 600,
+    easing: 'ease-in-sine'
   });
   /* подключение и настрока плагина анимации */
 })(jQuery);
