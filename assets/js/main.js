577bcfa59169f9fb53693be2c4794af848fb8a44
Andrey Fomenko <fomenkoandrey1978@gmail.com>
2019-05-02 18:01:41 +0300

    Добавление кнопки возврата в начало страницы.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,31 @@
 (function ($) {
   let $navbar = $('#header-navbar');
+  let $btnBack2Top = $('#back2top');
 
+  /* кнопка возврата в начало страницы */
+  const back2TopOffset = 700;
+  const back2TopAnimationDuration = 500;
+  const back2TopAnimationFunction = 'swing';
+  const back2Top = (pos) => pos >= back2TopOffset ? $btnBack2Top.fadeIn(500) : $btnBack2Top.fadeOut(500);
+
+  $btnBack2Top.on('click', () => {
+    $('html').animate({
+      scrollTop : 0
+    }, back2TopAnimationDuration, back2TopAnimationFunction);
+    return false;
+  });
+  /* кнопка возврата в начало страницы */
+  /* --------------------- */
   /* добавление класса-модификатора в меню на скрол */
   const headerClassOnScroll = 'header-navbar--scrolled';
   const scrollPos = 100;
 
   $(window).on('scroll', () => {
     $(window).scrollTop() > scrollPos ? $navbar.addClass(headerClassOnScroll) : $navbar.removeClass(headerClassOnScroll);
+    back2Top($(window).scrollTop());
   });
   /* добавление класса-модификатора в меню на скрол */
-
+  /* --------------------- */
   /* подключение и настройка внутристраничной навигации */
   $navbar.onePageNav({
     currentClass: 'active',
@@ -18,24 +34,24 @@
     filter: ':not(.navbar-brand)'
   });
   /* подключение и настройка внутристраничной навигации */
-
+  /* --------------------- */
   /* закрытие меню при клике */
-  let windowSize = $(window).width();
+  let windowWidth = $(window).width();
 
-  $(window).on('resize', () => windowSize = $(window).width());
+  $(window).on('resize', () => windowWidth = $(window).width());
 
-  let $link = $('#header-navbar-collapse a');
+  let $navbarLink = $('#header-navbar-collapse a');
   let $navbarToggler = $('.navbar-toggler');
   let $headerNavbarCollapse = $('#header-navbar-collapse');
 
-  $link.on('click', () => {
-    if (windowSize < 992) {
+  $navbarLink.on('click', () => {
+    if (windowWidth < 992) {
       $navbarToggler.addClass('collapsed');
       $headerNavbarCollapse.removeClass('show');
     }
   });
   /* закрытие меню при клике */
-
+  /* --------------------- */
   /* подключение и настрока плагина анимации */
   AOS.init({
     disable: 'mobile',
